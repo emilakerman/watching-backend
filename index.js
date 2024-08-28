a48d2961929d1977dc28856eec8477b270b1d680
emilakerman <emilaakerman@gmail.com>
2024-08-28 13:25:08 +0200

    added prettier formatting to github actions

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-import express from "express";
-import pkg from "pg";
-import { postPW, postHost, postDB, postUser, postPort, secretToken } from "./config.js";
+import express from 'express';
+import pkg from 'pg';
+import {
+  postPW,
+  postHost,
+  postDB,
+  postUser,
+  postPort,
+  secretToken,
+} from './config.js';
 
 const { Pool } = pkg;
 const app = express();
@@ -30,53 +37,56 @@ app.get(`/${secretToken}/settings`, async (req, res) => {
     res.json(result.rows);
   } catch (err) {
     console.error(err);
-    res.status(500).send("Error querying database");
+    res.status(500).send('Error querying database');
   }
 });
 
 // TODO: Test and implement endpoint in frontend!
 // Returns all featured items.
 app.get(`/${secretToken}/featured`, async (req, res) => {
-    try {
-        const result = await pool.query('SELECT * FROM "Featured"');
-        res.json(result.rows);
-    } catch (err) {
-        console.error(err);
-        res.status(500).send("Error querying database");
-    }
-    });
+  try {
+    const result = await pool.query('SELECT * FROM "Featured"');
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error querying database');
+  }
+});
 
 // Returns all users.
 // TODO: Test and implement endpoint in frontend!
 app.get(`/${secretToken}/users`, async (req, res) => {
-    try {
-        const result = await pool.query('SELECT * FROM "Users"');
-        res.json(result.rows);
-    } catch (err) {
-        console.error(err);
-        res.status(500).send("Error querying database");
-    }
-    });
+  try {
+    const result = await pool.query('SELECT * FROM "Users"');
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error querying database');
+  }
+});
 
 // Returns all publics users
 app.get(`/${secretToken}/public-users`, async (req, res) => {
-    try {
-        const result = await pool.query('SELECT * FROM "Settings" WHERE "isPublic" = true');
-        res.json(result.rows);
-    } catch (err) {
-        console.error(err);
-        res.status(500).send("Error querying database");
-    }
-    });
+  try {
+    const result = await pool.query(
+      'SELECT * FROM "Settings" WHERE "isPublic" = true'
+    );
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error querying database');
+  }
+});
 
 // Returns true or false if user exists.
 // TODO: Test and implement endpoint in frontend!
 app.get(`/${secretToken}/users/:userId`, async (req, res) => {
   const userId = req.params.userId;
   try {
-    const result = await pool.query('SELECT * FROM "Users" WHERE "userId" = $1', [
-      userId,
-    ]);
+    const result = await pool.query(
+      'SELECT * FROM "Users" WHERE "userId" = $1',
+      [userId]
+    );
     if (result.rows.length > 0) {
       res.json({ exists: true });
     } else {
@@ -84,10 +94,8 @@ app.get(`/${secretToken}/users/:userId`, async (req, res) => {
     }
   } catch (err) {
     console.error(err);
-    res.status(500).send("Error querying database");
+    res.status(500).send('Error querying database');
   }
 });
 
-
 export { app, server };
-
